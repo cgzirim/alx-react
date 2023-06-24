bdfc4ad84f85afe675dea9e75e95491445b08405
Chigozirim <iamokwukwe@gmail.com>
2023-06-24 23:33:36 +0100

    Create basic tests with four tests

---
+++ 0x01-react_intro/task_3/dashboard/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import Notifications from './Notifications';
+
+const root_notif = ReactDOM.createRoot(document.getElementById('root-notifications'));
+root_notif.render(
+  <React.StrictMode>
+    <Notifications />
+  </React.StrictMode>
+)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
